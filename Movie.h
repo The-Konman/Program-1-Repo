c2ec059a590050c2965a23330dd40bc94b1166bb
The-Konman <wlorance42@tntech.edu>
2024-09-20 15:28:57 -0500

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -4,23 +4,68 @@
 
 */
 
-#ifndef MOVIE_H
+#ifndef
 #define MOVIE_H
 
 class Movie {
 
     private:
-
-
+        string title;
+        string genre;
+        *Rating movierating;
+        int year;
+        int movieLength;
 
 
     public:
+        //Getters
+        string getTitle()const
+        {
+            return title;
+        }
+        string getGenre() const
+        {
+            return genre;
+        }
+        int getYear() const
+        {
+            return year;
+        }
+        int getMovieLength() const
+        {
+            return length;
+        }
+
+        //Setters
+        void setTitle(int t) 
+        {
+            title = t;
+        }
+        void setGenre(string g) 
+        {
+            genre = g;
+        }
+        void setYear(int y) 
+        {
+            year = y;
+        }
+        void setMovieLength(int m) 
+        {
+            movieLength = m;
+        }
+        //Overloaded constructor
+        Movie(string t, string g, int y, int m) 
+        { 
+            title = t;
+            genre = g;
+            year = y;
+            movieLength = m;
+        }
+        
+        
 
 
-
-
-};
-
+}
 
 
 #endif
