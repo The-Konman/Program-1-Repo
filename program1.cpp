b3764ee5585c90006d12749ebd148ca246d72ccf
Madeline Griffin <mcgriffin42@tntech.edu>
2024-09-27 13:54:41 -0500

    feat program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "DvdShelf.h"
 #include "Movie.h"
+#include "Rating.h"
 
 using namespace std;
 
@@ -16,6 +17,13 @@ int main ()
 {
     
     int choice = 0;
+    string title;
+    string genre;
+    int year;
+    int length;
+    Movie movie(title, genre, year, length);
+    
+
 
     cout << "\n\nWelcome to the Dvd Shelf" << endl<< endl;
 
@@ -24,7 +32,7 @@ int main ()
     cout << "3. Display Movies" << endl;
     cout << "4. Quit Program" << endl;
 
-    cout << "Pick an option from above";
+    cout << "Pick an option from above\t";
     cin >> choice;
 
     while (choice < 1 || choice > 4) {
@@ -38,6 +46,51 @@ int main ()
     while (choice != 4) {
 
         if (choice == 1) {
+
+            cout << "\n\nYou have chosen to add a movie." << endl;
+            cout << "What is the title of the movie you want to add?\t";
+
+            cin.ignore();
+            getline(cin, title);
+            movie.setTitle(title);
+
+            cout << "\n\nWhat genre is " << title << "?\t";
+            
+            getline(cin, genre);
+            movie.setGenre(genre);
+
+            cout << "\n\nWhat year did " << title << " come out?\t";
+
+            cin >> year;
+            movie.setYear(year);
+
+            cout << "\n\nHow long is " << title << " in minutes?\t";
+
+            cin >> length;
+            movie.setMovieLength(length);
+
+            cout << "\n\nYour movie has been added." << endl;
+
+            cout << "Title: " << movie.getTitle();
+            cout << "\nGenre: " << movie.getGenre();
+            cout << "\nYear: " << movie.getYear();
+            cout << "\nLength (in Minutes): " << movie.getMovieLength();
+
+
+            cout << "\n\n\nWhat would you like to do next?" << endl;
+            cout << "1. Add Movie" << endl;
+            cout << "2. Remove Movie" << endl;
+            cout << "3. Display Movies" << endl;
+            cout << "4. Quit Program" << endl;
+
+            cout << "Pick an option from above\t";
+            cin >> choice;
+
+            while (choice < 1 || choice > 4) {
+
+                cout << "\n\nPlease select a valid option";
+                cin >> choice;
+    }
 
         }
 
