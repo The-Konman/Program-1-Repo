a18baba4dfc7a7b6e19a0aa4ab084db85dc0bcc5
geovapor <gvaporis42@tntech.edu>
2024-10-01 11:23:39 -0500

    Update Rating.h

--- Rating.h
+++ Rating.h
@@ -3,18 +3,66 @@
 
 
 */
-
-#include "DvdShelf.h"
 #ifndef RATING_H
 #define RATING_H
 
-class Rating {
+class Rating 
+{
 
     private:
+
+    int movieRating;
+    int boxOffice;
+    int rottenTomatoScore;
 
 
 
     public:
+
+    int getMovieRating() const
+    {
+        return movieRating;
+
+    }
+
+    int getBoxOffice() const
+    {
+        return boxOffice;
+
+    }
+
+    int getRTScore() const
+    {
+        return rottenTomatoScore;
+    }
+    
+
+
+    void setMovieRating(double mR)
+    {
+        movieRating = mR;
+    }
+
+    void setBoxOffice(double bO)
+    {
+        boxOffice = bO;
+    }
+    
+    void setCriticReview(int rTS)
+    {
+        rottenTomatoScore = rTS;
+    }
+    
+
+
+
+    Rating(int mR, int bO, int rTS)
+    {
+        movieRating = mR;
+        boxOffice = bO;
+        rottenTomatoScore = rTS;
+        
+    }
 
 
 
